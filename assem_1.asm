9fdf26412d4e6db8c2d9b734313d85444918af01
shehry <shehrimary@gmail.com>
2026-03-25 12:23:33 +0500

    comments is added to the assem_1 file

--- assem_1.asm
+++ assem_1.asm
@@ -1,10 +1,10 @@
 [org 0x0100]
 
-mov ax, 5
-mov bx, 10
-add ax, bx
+mov ax, 5   ; move 5 to the accumulator register
+mov bx, 10  ;move 10 to the base register
+add ax, bx  ;adding the values in register ax and bx and storing it in ax
 mov bx, 12
 add ax, bx
 
-mov ax, 0x4c00
+mov ax, 0x4c00  ; exiting
 int 0x21
