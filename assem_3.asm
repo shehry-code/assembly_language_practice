f5b159d4a96ffa3ccc57544fe7d4c7179c59526a
shehry <shehrimary@gmail.com>
2026-03-29 21:35:06 +0500

    _

--- assem_3.asm
+++ assem_3.asm
@@ -9,11 +9,11 @@ start:
 	outerloop:
 	mov bx, 0
 		innerloop:
-			mov ax, [data + bx]
-			cmp ax, [data + bx + 2]
-			jbe noswap
+			mov ax, [data + bx]		; First value is moved to ax register
+			cmp ax, [data + bx + 2]		; Comparing 1st value with 2nd
+			jbe noswap		; Jump if below or equal
 		;swapping
-		mov dx, [data + bx + 2]
+		mov dx, [data + bx + 2]		; Second value is moved to dx register
 		mov [data + bx], dx
 		mov [data + bx + 2], ax
 	
